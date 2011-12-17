8347e044bc05e7d070f5aa49443b7b9a4e480893
Jan Suchal <johno@jsmf.net>
2011-12-17 15:51:39 +0100

    Comments.

--- app/helpers/timeline_helper.rb
+++ app/helpers/timeline_helper.rb
@@ -1,2 +1,5 @@
 module TimelineHelper
+  def gravatar_for(email, size = 80)
+    image_tag("http://www.gravatar.com/avatar/#{Digest::MD5::hexdigest(email)}?s=#{size}", alt: '', size: "#{size}x#{size}")
+  end
 end
