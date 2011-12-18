4a95f4b8adc73fb9596cf18bc988476cd3cba4d0
duze <dusan.zelenik@gmail.com>
2011-12-18 13:28:11 +0100

    at su tam tie mapy furt! povedal tomas

--- app/assets/javascripts/lil_map.js
+++ app/assets/javascripts/lil_map.js
@@ -1,32 +1,49 @@
 $(function() {
-    $('.map').click(function() {
-     var map_id = $(this).data('map-id');
-     var map_canvas_id = "map_canvas_" + map_id;
-     var location = $(this).data("map-location");
-     little_map(map_canvas_id, location);
-     return false;
+    $('.map').each(function() {
+        var map_id = $(this).data('map-id');
+        var map_canvas_id = "map_canvas_" + map_id;
+        //var location = $(this).data("map-location");
+        var location_lat = $(this).data("map-location-lat");
+        var location_lon = $(this).data("map-location-lon");
+        //little_map_label(map_canvas_id, location);
+        little_map_gps(map_canvas_id, location_lat, location_lon);
     });
 
-    function little_map(div_id, location_label) {
-      var geocoder;
-      var map;
-      geocoder = new google.maps.Geocoder();
-      var myOptions = {
-        zoom: 15,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-      }
-      map = new google.maps.Map(document.getElementById(div_id), myOptions);
-
-      geocoder.geocode( { 'address': location_label}, function(results, status) {
-        if (status == google.maps.GeocoderStatus.OK) {
-          map.setCenter(results[0].geometry.location);
-          var marker = new google.maps.Marker({
-              map: map,
-              position: results[0].geometry.location
-          });
-        } else {
-          //alert("Geocode was not successful for the following reason: " + status);
+    function little_map_gps(div_id, lat, lon){
+        var map;
+        var pos = new google.maps.LatLng(lat, lon);
+        var myOptions = {
+            zoom: 15,
+            center: pos,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
         }
-      });
+        map = new google.maps.Map(document.getElementById(div_id), myOptions);
+        var marker = new google.maps.Marker({
+            map: map,
+            position: pos
+        });
+    }
+
+    function little_map_label(div_id, location_label) {
+        var geocoder;
+        var map;
+        geocoder = new google.maps.Geocoder();
+        var myOptions = {
+            zoom: 15,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        }
+        map = new google.maps.Map(document.getElementById(div_id), myOptions);
+
+        geocoder.geocode( { 'address': location_label}, function(results, status) {
+            if (status == google.maps.GeocoderStatus.OK) {
+                map.setCenter(results[0].geometry.location);
+                var marker = new google.maps.Marker({
+                    map: map,
+                    position: results[0].geometry.location
+                });
+            } else {
+                //alert("Geocode was not successful for the following reason: " + status);
+            }
+        });
     }
 });
