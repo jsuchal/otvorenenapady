ce19cf0488df98f59e5ba1fe7996dec29986e503
duze <dusan.zelenik@gmail.com>
2011-12-18 12:37:35 +0100

    oprava migracie location_label update

--- db/migrate/20111218105715_update_event_location_label.rb
+++ db/migrate/20111218105715_update_event_location_label.rb
@@ -1,7 +1,7 @@
 class UpdateEventLocationLabel < ActiveRecord::Migration
   def up
     execute "UPDATE events SET location_label = regexp_replace(location_label, '\n', ', ')"
-    execute "UPDATE events SET location_label = location_label||', '||'Bratislava',  WHERE location_label not like '%Bratislava%'"
+    execute "UPDATE events SET location_label = location_label||', '||'Bratislava'  WHERE location_label not like '%Bratislava%'"
   end
 
   def down
