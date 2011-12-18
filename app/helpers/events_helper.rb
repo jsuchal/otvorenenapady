bbf2ec4a32c2541f515a115cb695ae894c63cd14
Tomas Kramar <kramar.tomas@gmail.com>
2011-12-18 14:01:47 +0100

    Show labels in popup

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,2 +1,16 @@
+#encoding: utf-8
 module EventsHelper
+  def event_category_label(event)
+    case event.category
+    when 'Media' then
+      title = "Médiá"
+    when 'Review' then
+      title = "Recenzie"
+    when 'Grant' then
+      title = "Granty"
+    when 'Discount' then
+      title = "Zľava"
+    end
+    "<span class='label category#{event.category}'>#{title}</span>"
+  end
 end
